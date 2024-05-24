c868a57772d30fac5e6bb7ac9ffea09c87476d45
AdamLesage <adamles44@gmail.com>
2024-05-24 14:59:07 +0200

    feat(agent connexion) client can receive data from server close #75

--- src/ai/Agent.py
+++ src/ai/Agent.py
@@ -12,17 +12,23 @@ class Agent():
         self.port = port
         self.team_name = team_name
         self.ip = ip
+        self.client = None
 
     def connect_to_server(self):
         """Connect to the server from the given ip and port"""
-        print(f"Connecting to {self.ip} on port {self.port} with team name {self.team_name}")
         try:
             self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.client.connect((self.ip, self.port))
-            print("Connected to the server")
+            while True:
+                data = self.client.recv(1024).decode()
+                print(f"Received: {data}")
+
+
         except Exception as e:
             print(f"Error: {e}")
             exit(84)
+
+
 
     def disconnect_from_server(self):
         """Disconnect from the server"""
