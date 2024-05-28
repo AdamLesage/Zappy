60192b27efeb771523bf37318130a23c5b40c28d
AdamLesage <adamles44@gmail.com>
2024-05-28 16:37:39 +0200

    fix signature encryption #70

--- src/ai/data_encryption.py
+++ src/ai/data_encryption.py
@@ -5,11 +5,12 @@
 ## data_encryption
 ##
 
-def encrypt_data(data: str, key: str) -> str:
+def encrypt_data(data: str, key: str, team_name: str) -> str:
     """Encrypt data with a given key"""
     encrypted_data = ""
-    for i in range(len(data)):
-        encrypted_data += chr(ord(data[i]) ^ ord(key[i % len(key)]))
+    to_crypt = f"{team_name}/{data}"
+    for i in range(len(to_crypt)):
+        encrypted_data += chr(ord(to_crypt[i]) ^ ord(key[i % len(key)]))
     return encrypted_data
 
 def decrypt_data(data: str, key: str) -> str:
@@ -18,8 +19,3 @@ def decrypt_data(data: str, key: str) -> str:
     for i in range(len(data)):
         decrypted_data += chr(ord(data[i]) ^ ord(key[i % len(key)]))
     return decrypted_data
-
-def add_signature(data: str, team_name: str) -> str:
-    """Add a signature to the data"""
-    crypted_team_name = encrypt_data(team_name, team_name)
-    return f"{crypted_team_name}/{data}"
