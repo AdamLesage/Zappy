4f2a35f32feacc39beeaab99ec9970384dcd4da4
AdamLesage <adamles44@gmail.com>
2024-05-28 18:50:02 +0200

    feat(coverage rule): if coverage is below 75% exit 1 to avoid untested code

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -9,14 +9,21 @@ $COVERAGE erase
 cd src/ai/tests
 $COVERAGE run -m unittest test_AgentInfo.py -v
 $COVERAGE run -m unittest test_Commands.py -v
-$COVERAGE report -m
+REPORT=$($COVERAGE report -m)
+echo "$REPORT"
 cd ../../../
 
 # Check the exit code
-if [ $? -eq 0 ]; then
-    echo "Exit code is 0."
-    exit 0
-else
-    echo "Exit code is not 0."
+if [ $? -eq 1 ]; then
+    exit 1
+fi
+
+# If coverage is inferior to 75%, exit with an error
+COVERAGE_PERCENTAGE=$(echo "$REPORT" | grep -oP "TOTAL.*" | grep -oP "\d+%" | grep -oP "\d+")
+
+# echo "$COVERAGE_PERCENTAGE"
+
+if [ $COVERAGE_PERCENTAGE -lt 75 ]; then
+    echo "Coverage is below 75%: $COVERAGE_PERCENTAGE"
     exit 1
 fi
