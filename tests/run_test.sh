7747a708471e05d7a1df34cb26fd8ad84909b036
AdamLesage <adamles44@gmail.com>
2024-05-30 11:09:18 +0200

    test(Bash script tester) fix script to check coverage of each test file

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -5,25 +5,30 @@ COVERAGE=$(python3 -m site --user-base)/bin/coverage
 
 $COVERAGE erase
 
-# Execute tests
 cd src/ai/tests
-$COVERAGE run -m unittest test_AgentInfo.py -v
-$COVERAGE run -m unittest test_Commands.py -v
-REPORT=$($COVERAGE report -m)
-echo "$REPORT"
-cd ../../../
 
-# Check the exit code
-if [ $? -eq 1 ]; then
-    exit 1
-fi
+# Define the test files
+TEST_FILES=("test_AgentInfo.py" "test_Commands.py")
 
-# If coverage is inferior to 75%, exit with an error
-COVERAGE_PERCENTAGE=$(echo "$REPORT" | grep -oP "TOTAL.*" | grep -oP "\d+%" | grep -oP "\d+")
+# Execute tests and check coverage for each file
+for file in "${TEST_FILES[@]}"
+do
+    $COVERAGE run -m unittest "$file" -v
+    REPORT=$($COVERAGE report -m)
+    echo "$REPORT"
 
-# echo "$COVERAGE_PERCENTAGE"
+    # Check the exit code
+    if [ $? -eq 1 ]; then
+        exit 1
+    fi
 
-if [ $COVERAGE_PERCENTAGE -lt 75 ]; then
-    echo "Coverage is below 75%: $COVERAGE_PERCENTAGE"
-    exit 1
-fi
+    # If coverage is inferior to 75%, exit with an error
+    COVERAGE_PERCENTAGE=$(echo "$REPORT" | grep -oP "TOTAL.*" | grep -oP "\d+%" | grep -oP "\d+")
+
+    if [ $COVERAGE_PERCENTAGE -lt 75 ]; then
+        echo "Coverage is below 75%: $COVERAGE_PERCENTAGE"
+        exit 1
+    else
+        echo "Coverage is above 75%: $COVERAGE_PERCENTAGE"
+    fi
+done
