838d0cd4654527499dd949eb35f222e3f524915f
Victor Hristea <victorhristea@gmail.com>
2024-05-30 17:34:23 +0200

    feat(GUI class): add the class of Inventory for players and Tiles

---
+++ src/gui/Inventory.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** Inventory
+*/
+
+#ifndef INVENTORY_HPP_
+#define INVENTORY_HPP_
+
+#include "AInventory.hpp"
+
+class Inventory : public AInventory{
+    public:
+        Inventory();
+        ~Inventory();
+
+    protected:
+    private:
+};
+
+#endif /* !INVENTORY_HPP_ */
