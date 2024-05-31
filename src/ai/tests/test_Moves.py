a2fbb1a81abf7fb768c373435cb008e36e863c55
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-05-31 18:56:47 +0200

    test (tests): add tests to tests the checkItem method from the Moves class

---
+++ src/ai/tests/test_Moves.py
@@ -0,0 +1,29 @@
+##
+## EPITECH PROJECT, 2024
+## Zappy
+## File description:
+## test_Moves
+##
+
+# Python imports
+import sys
+sys.path.append('..')
+import unittest
+from unittest.mock import patch
+import importlib.util
+
+# Project imports
+from models.Moves import Moves
+
+class TestMovesClass(unittest.TestCase):
+    def test01_checkItem(self):
+        """Test the checkItem methods"""
+        testMoves = Moves()
+        self.assertEqual(testMoves.checkItem("[player,,,thystame,,food,,,,,thystame,,,,,,]", "food"), True)
+        return
+    
+    def test02_checkItem(self):
+        """Test the checkItem methods"""
+        testMoves = Moves()
+        self.assertEqual(testMoves.checkItem("[player,,,thystame,,food,,,,,thystame,,,,,,]", "linemate"), False)
+        return
