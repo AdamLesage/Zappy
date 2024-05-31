2a1dbea868838d9dbd8407992d422f7a5a3b1a5a
rchevallier1234 <romain.chevallier@epitech.eu>
2024-05-31 16:58:19 +0200

    feat(Server Command): add is_know_player_command function close #106

--- src/server/command/Player_command/is_know_player_command.c
+++ src/server/command/Player_command/is_know_player_command.c
@@ -7,7 +7,19 @@
 
 #include "server.h"
 
+const command_list_t commands_player_list[];
+
 bool is_know_player_command(char *command)
 {
-    return (true);
+    char **array_command = my_str_to_word_array(command, ' ');
+
+    if (array_command == NULL) {
+        return (false);
+    }
+    for (int i = 0; commands_player_list[i].name != NULL; i++) {
+        if (strcmp(commands_player_list[i].name, array_command[0]) == 0) {
+            return (true);
+        }
+    }
+    return (false);
 }
