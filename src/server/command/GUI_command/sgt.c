2ad7273f429ba7071592a6c2d2f94673b7b1d0dd
SoahkrapS <nicolas2.bertrand@epitech.eu>
2024-06-05 17:18:08 +0200

    feat(Server command GUI): add sgt command

--- src/server/command/GUI_command/sgt.c
+++ src/server/command/GUI_command/sgt.c
@@ -9,5 +9,20 @@
 
 void sgt(core_t *core, int fd, char **command)
 {
-    printf("sgt\n");
+    char *buff = NULL;
+    int buff_size = 0;
+
+    if (command == NULL || len_array(command) != 1) {
+        send_response("sbp\n", fd);
+        return;
+    }
+    buff_size = strlen("sgt ") + strlen(int_to_str(core->arguments.frequency))
+    + strlen("\n") + 1;
+    buff = malloc(sizeof(char) * buff_size);
+    buff[0] = '\0';
+    strcat(buff, "sgt ");
+    strcat(buff, int_to_str(core->arguments.frequency));
+    strcat(buff, "\n");
+    send_response(buff, fd);
+    free(buff);
 }
