4849609806aa831c4dd5b71413e4ef7057e3b3dd
AdamLesage <adamles44@gmail.com>
2024-06-05 14:55:09 +0200

    feat(PPO command GUI): add command PPO, can retrieve and ask data

--- src/gui/ServerInfo/Command/PPO.cpp
+++ src/gui/ServerInfo/Command/PPO.cpp
@@ -22,11 +22,17 @@ T Zappy::PPO::execute(std::string message)
         return;
 
     std::vector<std::string> args = my_str_to_word_array(message.c_str());
+    if (args.size() != 5)
+        throw std::invalid_argument("Invalid number of arguments for PPO command");
+
+    // return format std::vector<std::string> { "ppo", "playerNumber", "x_pos", "y_pos", "orientation" };
+    std::string response = "ppo " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + "\n";
+
 }
 
 void Zappy::PPO::askCommand(int fd, std::vector<std::string> args)
 {
-    
-    std::string message = "ppo\n"; // add player number
+    std::string playerNumber = args[1];
+    std::string message = "ppo " + playerNumber + "\n";
     write(fd, message.c_str(), message.size());
 }
