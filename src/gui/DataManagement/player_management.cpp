5b38f705986fc406ca7ebda7830d7dcaa3e2f100
mathieu-MZ <mathieu.mazeau@epitech.eu>
2024-06-06 14:04:58 +0200

    fix(GUI datamanagement): fix warning for player management

--- src/gui/DataManagement/player_management.cpp
+++ src/gui/DataManagement/player_management.cpp
@@ -11,61 +11,62 @@ namespace Zappy {
     namespace GuiConnect {
         void createPlayer(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void deletePlayer(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void updatePlayerCoords(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void getPlayerCoords(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void updatePlayerInventory(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void createPlayerInventory(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void getPlayerInventory(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void getPlayerTeam(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void updatePlayerTeam(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void updatePlayerLevel(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void getPlayerLevel(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         std::shared_ptr<Zappy::Player> getPlayerByNb(int playerNb)
         {
+            playerNb = playerNb;
             return nullptr;
         }
     }
