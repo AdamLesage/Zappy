57ae38219ee8073152c8505bb8c166bb9de0f739
AdamLesage <adamles44@gmail.com>
2024-06-06 13:52:55 +0200

    feat(Egg class): create egg class which inherit from player class

---
+++ src/gui/Entity/Egg.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** Egg
+*/
+
+#ifndef EGG_HPP_
+#define EGG_HPP_
+
+#include "APlayer.hpp"
+
+namespace Zappy {
+    class Egg : public APlayer {
+        public:
+            Egg();
+            ~Egg();
+
+        protected:
+        private:
+    };
+}
+
+#endif /* !EGG_HPP_ */
