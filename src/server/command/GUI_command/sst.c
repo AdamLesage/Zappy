6f904f7a173a2cafa8af4ec796c948dfa14a507b
SoahkrapS <nicolas2.bertrand@epitech.eu>
2024-06-06 15:25:09 +0200

    feat(Server command GUI): add sst command

--- src/server/command/GUI_command/sst.c
+++ src/server/command/GUI_command/sst.c
@@ -9,5 +9,22 @@
 
 void sst(core_t *core, int fd, char **command)
 {
-    printf("sst\n");
+    char *buff = NULL;
+    int buff_size = 0;
+    int new_frequency = 0;
+
+    if (len_array(command) != 2) {
+        send_response("sbp\n", fd);
+        return;
+    }
+    new_frequency = atoi(command[1]);
+    core->arguments.frequency = new_frequency;
+    buff_size = strlen("sst ") + strlen(command[1]) + strlen("\n") + 1;
+    buff = malloc(sizeof(char) * buff_size);
+    buff[0] = '\0';
+    strcat(buff, "sst ");
+    strcat(buff, command[1]);
+    strcat(buff, "\n");
+    send_response(buff, fd);
+    free(buff);
 }
