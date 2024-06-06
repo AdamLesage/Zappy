b1f6a723724cb86addb6e5b22af4719a6110bc77
mathieu-MZ <mathieu.mazeau@epitech.eu>
2024-06-06 14:04:47 +0200

    fix(GUI datamanagement): fix warning for egg management

--- src/gui/DataManagement/egg_management.cpp
+++ src/gui/DataManagement/egg_management.cpp
@@ -11,21 +11,22 @@ namespace Zappy {
     namespace GuiConnect {
         void createEgg(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void deleteEgg(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         void updateEgg(std::vector<std::string> args)
         {
-
+            args = args;
         }
 
         std::shared_ptr<Egg> getEggByNb(int eggNb)
         {
+            eggNb = eggNb;
             return nullptr;
         }
     }
