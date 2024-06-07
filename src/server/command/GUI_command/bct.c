27249046b72b8c70198f5b1ac0a1dc948b302cab
SoahkrapS <nicolas2.bertrand@epitech.eu>
2024-06-07 11:27:35 +0200

    fix(Server command GUI): fix bct command

--- src/server/command/GUI_command/bct.c
+++ src/server/command/GUI_command/bct.c
@@ -7,11 +7,24 @@
 
 #include "server.h"
 
+tiles_list_t *get_current_tile(core_t *core, int x, int y)
+{
+    tiles_list_t *current_tile = core->map.tiles_list;
+
+    while (current_tile != NULL) {
+        if (current_tile->pos_x == x && current_tile->pos_y == y) {
+            return current_tile;
+        }
+        current_tile = current_tile->next;
+    }
+    return NULL;
+}
+
 void bct(core_t *core, int fd, char **command)
 {
     int x = atoi(command[1]);
     int y = atoi(command[2]);
-    tiles_list_t *current_tile = core->map.tiles_list;
+    tiles_list_t *current_tile = get_current_tile(core, x, y);
 
     if (len_array(command) != 3) {
         send_response("sbp\n", fd);
@@ -35,6 +48,8 @@ void bct_two(core_t *core, int fd, tiles_list_t *current_tile)
     send_response(" ", fd);
     send_response_int(current_tile->pos_y, fd);
     send_response(" ", fd);
+    send_response_int(tile_info->nb_food, fd);
+    send_response(" ", fd);
     send_response_int(tile_info->nb_linemate, fd);
     send_response(" ", fd);
     send_response_int(tile_info->nb_deraumere, fd);
@@ -43,8 +58,6 @@ void bct_two(core_t *core, int fd, tiles_list_t *current_tile)
     send_response(" ", fd);
     send_response_int(tile_info->nb_mendiane, fd);
     send_response(" ", fd);
-    send_response_int(tile_info->nb_phiras, fd);
-    send_response(" ", fd);
     bct_three(core, fd, current_tile);
 }
 
@@ -52,8 +65,8 @@ void bct_three(core_t *core, int fd, tiles_list_t *current_tile)
 {
     tile_info_t *tile_info = current_tile->tile_info;
 
+    send_response_int(tile_info->nb_phiras, fd);
+    send_response(" ", fd);
     send_response_int(tile_info->nb_thystame, fd);
-    send_response(" ", fd);
-    send_response_int(tile_info->nb_food, fd);
-    send_response(" ", fd);
+    send_response("\n", fd);
 }
--- tests/server/test_command_GUI.c
+++ tests/server/test_command_GUI.c
@@ -58,3 +58,21 @@ Test(sst, test_sst, .init = cr_redirect_stdout)
     sst(&core, 1, command);
     cr_assert_stdout_eq_str(expected);
 }
+
+Test(bct, test_bct, .init = cr_redirect_stdout) {
+    core_t core;
+    char *command[] = {"bct", "5", "4", NULL};
+    char *expected = "bct 5 4 0 0 0 0 0 0 0\n";
+
+    core.map.width = 10;
+    core.map.height = 10;
+    tiles_list_t tile;
+    tile.pos_x = 5;
+    tile.pos_y = 4;
+    tile_info_t tile_info = {0, 0, 0, 0, 0, 0, 0};
+    tile.tile_info = &tile_info;
+    tile.next = NULL;
+    core.map.tiles_list = &tile;
+    bct(&core, 1, command);
+    cr_assert_stdout_eq_str(expected);
+}
