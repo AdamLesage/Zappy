9250a490c57e0e99dacd09770f0fed370beaa784
rchevallier1234 <romain.chevallier@epitech.eu>
2024-06-07 15:17:27 +0200

    test(Server Command player): add test for Right and Left Command

--- tests/server/test_command_move_player.c
+++ tests/server/test_command_move_player.c
@@ -13,13 +13,12 @@ Test (command_move_player, command_forward_N)
 {
     core_t core;
     player_info_t *info = NULL;
-    bool return_value = false;
 
     const char *argv[] = {"./zappy_server", "-p", "4242", "-x", "10", "-y", "10", "-n", "team1", "team2", "-c", "5", "-f", "100", NULL};
     int argc = 12;
 
     init_core(argc, argv, &core);
-    return_value = add_player(&core.map, &core.players, 2, "team1");
+    add_player(&core.map, &core.players, 2, "team1");
     info = find_player(&core.players, 2);
     info->orientation = N;
     info->pos_x = 5;
@@ -32,20 +31,19 @@ Test (command_move_player, command_forward_N)
     cr_assert_eq(info->pos_x, 5);
     cr_assert_eq(info->pos_y, 4);
     cr_assert_eq(info->orientation, N);
-    return_value = delete_player(&core.map, &core.players, 2);
+    delete_player(&core.map, &core.players, 2);
 }
 
 Test (command_move_player, command_forward_N_cross_map)
 {
     core_t core;
     player_info_t *info = NULL;
-    bool return_value = false;
 
     const char *argv[] = {"./zappy_server", "-p", "4242", "-x", "10", "-y", "10", "-n", "team1", "team2", "-c", "5", "-f", "100", NULL};
     int argc = 12;
 
     init_core(argc, argv, &core);
-    return_value = add_player(&core.map, &core.players, 2, "team1");
+    add_player(&core.map, &core.players, 2, "team1");
     info = find_player(&core.players, 2);
     info->orientation = N;
     info->pos_x = 5;
@@ -58,20 +56,19 @@ Test (command_move_player, command_forward_N_cross_map)
     cr_assert_eq(info->pos_x, 5);
     cr_assert_eq(info->pos_y, 9);
     cr_assert_eq(info->orientation, N);
-    return_value = delete_player(&core.map, &core.players, 2);
+    delete_player(&core.map, &core.players, 2);
 }
 
 Test (command_move_player, command_forward_S)
 {
     core_t core;
     player_info_t *info = NULL;
-    bool return_value = false;
 
     const char *argv[] = {"./zappy_server", "-p", "4242", "-x", "10", "-y", "10", "-n", "team1", "team2", "-c", "5", "-f", "100", NULL};
     int argc = 12;
 
     init_core(argc, argv, &core);
-    return_value = add_player(&core.map, &core.players, 2, "team1");
+    add_player(&core.map, &core.players, 2, "team1");
     info = find_player(&core.players, 2);
     info->orientation = S;
     info->pos_x = 5;
@@ -84,20 +81,19 @@ Test (command_move_player, command_forward_S)
     cr_assert_eq(info->pos_x, 5);
     cr_assert_eq(info->pos_y, 6);
     cr_assert_eq(info->orientation, S);
-    return_value = delete_player(&core.map, &core.players, 2);
+    delete_player(&core.map, &core.players, 2);
 }
 
 Test (command_move_player, command_forward_S_cross_map)
 {
     core_t core;
     player_info_t *info = NULL;
-    bool return_value = false;
 
     const char *argv[] = {"./zappy_server", "-p", "4242", "-x", "10", "-y", "10", "-n", "team1", "team2", "-c", "5", "-f", "100", NULL};
     int argc = 12;
 
     init_core(argc, argv, &core);
-    return_value = add_player(&core.map, &core.players, 2, "team1");
+    add_player(&core.map, &core.players, 2, "team1");
     info = find_player(&core.players, 2);
     info->orientation = S;
     info->pos_x = 5;
@@ -110,20 +106,19 @@ Test (command_move_player, command_forward_S_cross_map)
     cr_assert_eq(info->pos_x, 5);
     cr_assert_eq(info->pos_y, 0);
     cr_assert_eq(info->orientation, S);
-    return_value = delete_player(&core.map, &core.players, 2);
+    delete_player(&core.map, &core.players, 2);
 }
 
 Test (command_move_player, command_forward_E)
 {
     core_t core;
     player_info_t *info = NULL;
-    bool return_value = false;
 
     const char *argv[] = {"./zappy_server", "-p", "4242", "-x", "10", "-y", "10", "-n", "team1", "team2", "-c", "5", "-f", "100", NULL};
     int argc = 12;
 
     init_core(argc, argv, &core);
-    return_value = add_player(&core.map, &core.players, 2, "team1");
+    add_player(&core.map, &core.players, 2, "team1");
     info = find_player(&core.players, 2);
     info->orientation = E;
     info->pos_x = 5;
@@ -136,20 +131,19 @@ Test (command_move_player, command_forward_E)
     cr_assert_eq(info->pos_x, 6);
     cr_assert_eq(info->pos_y, 5);
     cr_assert_eq(info->orientation, E);
-    return_value = delete_player(&core.map, &core.players, 2);
+    delete_player(&core.map, &core.players, 2);
 }
 
 Test (command_move_player, command_forward_E_cross_map)
 {
     core_t core;
     player_info_t *info = NULL;
-    bool return_value = false;
 
     const char *argv[] = {"./zappy_server", "-p", "4242", "-x", "10", "-y", "10", "-n", "team1", "team2", "-c", "5", "-f", "100", NULL};
     int argc = 12;
 
     init_core(argc, argv, &core);
-    return_value = add_player(&core.map, &core.players, 2, "team1");
+    add_player(&core.map, &core.players, 2, "team1");
     info = find_player(&core.players, 2);
     info->orientation = E;
     info->pos_x = 9;
@@ -162,20 +156,19 @@ Test (command_move_player, command_forward_E_cross_map)
     cr_assert_eq(info->pos_x, 0);
     cr_assert_eq(info->pos_y, 5);
     cr_assert_eq(info->orientation, E);
-    return_value = delete_player(&core.map, &core.players, 2);
+    delete_player(&core.map, &core.players, 2);
 }
 
 Test (command_move_player, command_forward_W)
 {
     core_t core;
     player_info_t *info = NULL;
-    bool return_value = false;
 
     const char *argv[] = {"./zappy_server", "-p", "4242", "-x", "10", "-y", "10", "-n", "team1", "team2", "-c", "5", "-f", "100", NULL};
     int argc = 12;
 
     init_core(argc, argv, &core);
-    return_value = add_player(&core.map, &core.players, 2, "team1");
+    add_player(&core.map, &core.players, 2, "team1");
     info = find_player(&core.players, 2);
     info->orientation = W;
     info->pos_x = 5;
@@ -188,20 +181,19 @@ Test (command_move_player, command_forward_W)
     cr_assert_eq(info->pos_x, 4);
     cr_assert_eq(info->pos_y, 5);
     cr_assert_eq(info->orientation, W);
-    return_value = delete_player(&core.map, &core.players, 2);
+    delete_player(&core.map, &core.players, 2);
 }
 
 Test (command_move_player, command_forward_W_cross_map)
 {
     core_t core;
     player_info_t *info = NULL;
-    bool return_value = false;
 
     const char *argv[] = {"./zappy_server", "-p", "4242", "-x", "10", "-y", "10", "-n", "team1", "team2", "-c", "5", "-f", "100", NULL};
     int argc = 12;
 
     init_core(argc, argv, &core);
-    return_value = add_player(&core.map, &core.players, 2, "team1");
+    add_player(&core.map, &core.players, 2, "team1");
     info = find_player(&core.players, 2);
     info->orientation = W;
     info->pos_x = 0;
@@ -214,5 +206,51 @@ Test (command_move_player, command_forward_W_cross_map)
     cr_assert_eq(info->pos_x, 9);
     cr_assert_eq(info->pos_y, 5);
     cr_assert_eq(info->orientation, W);
-    return_value = delete_player(&core.map, &core.players, 2);
+    delete_player(&core.map, &core.players, 2);
+}
+
+Test (command_move_player, command_Right)
+{
+    core_t core;
+    player_info_t *info = NULL;
+
+    const char *argv[] = {"./zappy_server", "-p", "4242", "-x", "10", "-y", "10", "-n", "team1", "team2", "-c", "5", "-f", "100", NULL};
+    int argc = 12;
+
+    init_core(argc, argv, &core);
+    add_player(&core.map, &core.players, 2, "team1");
+    info = find_player(&core.players, 2);
+    info->orientation = W;
+    info->pos_x = 5;
+    info->pos_y = 5;
+    info->action_queue[0] = strdup("Right");
+    info->timer_action = 0;
+    check_player_command(&core);
+    cr_assert_eq(info->pos_x, 5);
+    cr_assert_eq(info->pos_y, 5);
+    cr_assert_eq(info->orientation, N);
+    delete_player(&core.map, &core.players, 2);
+}
+
+Test (command_move_player, command_Left)
+{
+    core_t core;
+    player_info_t *info = NULL;
+
+    const char *argv[] = {"./zappy_server", "-p", "4242", "-x", "10", "-y", "10", "-n", "team1", "team2", "-c", "5", "-f", "100", NULL};
+    int argc = 12;
+
+    init_core(argc, argv, &core);
+    add_player(&core.map, &core.players, 2, "team1");
+    info = find_player(&core.players, 2);
+    info->orientation = S;
+    info->pos_x = 5;
+    info->pos_y = 5;
+    info->action_queue[0] = strdup("Left");
+    info->timer_action = 0;
+    check_player_command(&core);
+    cr_assert_eq(info->pos_x, 5);
+    cr_assert_eq(info->pos_y, 5);
+    cr_assert_eq(info->orientation, E);
+    delete_player(&core.map, &core.players, 2);
 }
