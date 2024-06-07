e3728308ae92ea881b1514f4f1b7d0011db96fa8
SoahkrapS <nicolas2.bertrand@epitech.eu>
2024-06-07 14:27:53 +0200

    test(Server command GUI): test mct command

--- tests/server/test_command_GUI.c
+++ tests/server/test_command_GUI.c
@@ -59,7 +59,8 @@ Test(sst, test_sst, .init = cr_redirect_stdout)
     cr_assert_stdout_eq_str(expected);
 }
 
-Test(bct, test_bct, .init = cr_redirect_stdout) {
+Test(bct, test_bct, .init = cr_redirect_stdout)
+{
     core_t core;
     char *command[] = {"bct", "5", "4", NULL};
     char *expected = "bct 5 4 0 0 0 0 0 0 0\n";
@@ -74,5 +75,32 @@ Test(bct, test_bct, .init = cr_redirect_stdout) {
     tile.next = NULL;
     core.map.tiles_list = &tile;
     bct(&core, 1, command);
+    cr_assert_stdout_eq_str(expected);
+}
+
+Test(mct, test_mct, .init = cr_redirect_stdout)
+{
+    core_t core;
+    char *command[] = {"mct", NULL};
+    char *expected = "bct 0 0 0 0 0 0 0 0 0\nbct 0 1 0 0 0 0 0 0 0\n";
+
+    core.map.width = 10;
+    core.map.height = 10;
+    tiles_list_t tile;
+    tile.pos_x = 0;
+    tile.pos_y = 0;
+    tile_info_t tile_info = {0, 0, 0, 0, 0, 0, 0};
+    tile.tile_info = &tile_info;
+    tile.next = NULL;
+    core.map.tiles_list = &tile;
+
+    tiles_list_t tile2;
+    tile2.pos_x = 0;
+    tile2.pos_y = 1;
+    tile_info_t tile_info2 = {0, 0, 0, 0, 0, 0, 0};
+    tile2.tile_info = &tile_info2;
+    tile2.next = NULL;
+    tile.next = &tile2;
+    mct(&core, 1, command);
     cr_assert_stdout_eq_str(expected);
 }
