1e5bb73188d2704fc16d16cf9af96858c29f3864
mathieu-MZ <mathieu.mazeau@epitech.eu>
2024-06-07 10:34:24 +0200

    feat(GUI Command): add the PNW command

--- src/gui/ServerInfo/Command/PNW.cpp
+++ src/gui/ServerInfo/Command/PNW.cpp
@@ -14,3 +14,21 @@ Zappy::PNW::PNW()
 Zappy::PNW::~PNW()
 {
 }
+
+
+void Zappy::PNW::applyChanges(std::vector<std::string> parsedData,
+                                std::array<int, 2> &size_map,
+                                std::vector<std::vector<std::shared_ptr<Zappy::Tile>>> &tiles,
+                                std::vector<std::shared_ptr<Zappy::Player>> &players, 
+                                std::vector<std::shared_ptr<Zappy::Egg>> &eggs,
+                                int timeUnit,
+                                bool isRunning
+)
+{
+    std::shared_ptr<Player> player = std::make_shared<Player>();
+    player->setPlayerNumber(std::stoi(parsedData[1]));
+    player->setPosition(std::stoi(parsedData[2]), std::stoi(parsedData[3]));
+    player->setOrientation(std::stoi(parsedData[4]));
+    player->setPlayerLevel(std::stoi(parsedData[5]));
+    player->setTeamName(parsedData[6]);
+}
