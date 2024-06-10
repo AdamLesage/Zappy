630bd2ad1ba1187f681817d4813fa85f0076f6b2
SoahkrapS <nicolas2.bertrand@epitech.eu>
2024-06-10 17:26:21 +0200

    fix(Server command GUI): fix pin command test

--- tests/server/test_command_GUI.c
+++ tests/server/test_command_GUI.c
@@ -105,26 +105,26 @@ Test(mct, test_mct, .init = cr_redirect_stdout)
     cr_assert_stdout_eq_str(expected);
 }
 
-// Test(pin, test_pin, .init = cr_redirect_stdout)
-// {
-//     core_t core;
-//     char *command[] = {"pin", "1", NULL};
-//     char *expected = "pin 1 0 0 0 0 0 0 0 0 0\n";
+Test(pin, test_pin, .init = cr_redirect_stdout)
+{
+    core_t core;
+    char *command[] = {"pin", "1", NULL};
+    char *expected = "pin 1 0 0 0 0 0 0 0 0 0\n";
 
-//     core.map.width = 10;
-//     core.map.height = 10;
-//     players_list_t player;
-//     player.fd = 1;
-//     inventory_t inventory = {0, 0, 0, 0, 0, 0, 0};
-//     player_info_t player_info = {0, 0, 1, &inventory, {NULL}, NULL, 0, N, 1, "team1", 0};
+    core.map.width = 10;
+    core.map.height = 10;
+    players_list_t player;
+    player.fd = 1;
+    inventory_t inventory = {0, 0, 0, 0, 0, 0, 0};
+    player_info_t player_info = {0, 0, 1, &inventory, {NULL}, NULL, 0, N, 1, "team1", 0, NULL, 1};
 
-//     player.player_info = &player_info;
-//     player.next = NULL;
-//     core.players.players_list = &player;
+    player.player_info = &player_info;
+    player.next = NULL;
+    core.players.players_list = &player;
     
-//     pin(&core, 1, command);
-//     cr_assert_stdout_eq_str(expected);
-// }
+    pin(&core, 1, command);
+    cr_assert_stdout_eq_str(expected);
+}
 
 Test(ppo, test_ppo, .init = cr_redirect_stdout)
 {
