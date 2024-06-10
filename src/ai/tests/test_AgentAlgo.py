a4d7068ec53b14f037347fbb95ab1d6d56e29210
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-06-10 16:47:32 +0200

    test (tests AgentAlgo): add tests to test agentAlgo methods

--- src/ai/tests/test_AgentAlgo.py
+++ src/ai/tests/test_AgentAlgo.py
@@ -43,3 +43,31 @@ class TestAgentAlgo(unittest.TestCase):
         agentAlgo.setReturnCommandAnswer("ok")
         agentAlgo.clearReturnCommand()
         self.assertEqual(agentAlgo.getReturnCommand()[1], None)
+    
+    def test05_testUpdateInventory(self):
+        agent = AgentInfo()
+        agentAlgo = AgentAlgo(agent, 100)
+        agentAlgo.updateInventory("food 1")
+        self.assertEqual(agent.getInventory("food"), 1)
+    
+    def test06_testUpdateInventory(self):
+        agent = AgentInfo()
+        agentAlgo = AgentAlgo(agent, 100)
+        agentAlgo.updateInventory("linemate 2")
+        self.assertEqual(agent.getInventory("linemate"), 2)
+    
+    def test07_testUpdateInventory(self):
+        list = ["food 1, linemate 6, deraumere 3, sibur 4"]
+        agent = AgentInfo()
+        agentAlgo = AgentAlgo(agent, 100)
+        for elem in list:
+            agentAlgo.updateInventory(elem)
+        self.assertEqual(agent.getInventory("linemate"), 6)
+    
+    def test08_testUpdateInventory(self):
+        list = ["food 1, linemate 6, deraumere 3, sibur 4, thystame 1"]
+        agent = AgentInfo()
+        agentAlgo = AgentAlgo(agent, 100)
+        for elem in list:
+            agentAlgo.updateInventory(elem)
+        self.assertEqual(agent.getInventory("phiras"), 0)
