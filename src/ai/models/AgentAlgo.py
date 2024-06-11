73f91ec5bbb72b544cc640df6df484129acd72b3
AdamLesage <adamles44@gmail.com>
2024-06-11 11:22:51 +0200

    feat(Player pick): add function which return best pick according to player level

--- src/ai/models/AgentAlgo.py
+++ src/ai/models/AgentAlgo.py
@@ -77,7 +77,7 @@ class AgentAlgo():
         """
         if self.agentInfo.getLevel() != 1:
             return
-        pass
+        self.foodMode() # Search for food
 
     def clientPlayLevel2(self) -> None:
         """
@@ -181,11 +181,30 @@ class AgentAlgo():
         return
 
     def foodMode(self) -> None:
+        """
+        The agent is in food mode, it will search for food
+        """
         actions = ["Forward\n", "Right\n", "Forward\n", "Left\n"] # More chance to go forward (x2)
 
         buf = self.getReturnCommand()[1]
         if self.agentMoves.checkItem(self.getReturnCommand()[1], "food"):
             self.agentInfo.movements = self.agentMoves.reachItemList("food", buf)
+            return
+        else:
+            finalAction = random.choice(actions)
+            self.agentInfo.addCommandsToSend(finalAction)
+            return
+
+    def miningMode(self) -> None:
+        """
+        The agent is in mining mode, it will search for resources
+        """
+        actions = ["Forward\n", "Right\n", "Forward\n", "Left\n"] # More chance to go forward (x2)
+        buf = self.getReturnCommand()[1]
+        availables_resources = self.agentMoves.checkItems(buf, ["linemate", "deraumere", "sibur", "mendiane", "phiras", "thystame"])
+        if availables_resources != []: # If there are resources to take
+            item = random.choice(availables_resources)
+            self.agentInfo.movements = self.agentMoves.reachItemList(item, buf)
             return
         else:
             finalAction = random.choice(actions)
@@ -242,10 +261,7 @@ class AgentAlgo():
             return None
         if self.agentInfo.commandsToSend == deque([]) or self.client == None: # If there are no commands to send, get out of the function
             return None
-        print(f"LEN: {len(self.agentInfo.commandsToSend)} before sending")
         command_to_send = self.agentInfo.commandsToSend.popleft()
-        print(f"LEN: {len(self.agentInfo.commandsToSend)} after sending")
-        print(f"Sending command: {command_to_send}")
         self.client.send(command_to_send.encode())
         tmp = [command_to_send, ""]
         self.agentInfo.commandsReturned = tmp
@@ -267,15 +283,6 @@ class AgentAlgo():
             self.agentInfo.addCommandsToSend(command) # Add the command to the list of commands to send
         else:
             self.agentInfo.commandWaitingList.append(command)
-    
-    #def receiveFromServer(self) -> str:
-    #    """Receive a message from the server"""
-    #    self.client.setblocking(0)
-    #    try:
-    #        receivedData = self.client.recv(1024).decode()
-    #    except BlockingIOError:
-    #        return None
-    #    return receivedData
 
     def setReturnCommandAnswer(self, serverAnswer: str) -> None:
         """Set the command to return"""
@@ -288,3 +295,61 @@ class AgentAlgo():
     def clearReturnCommand(self) -> None:
         """Clear the command to return"""
         self.agentInfo.commandsReturned = [None, None]
+
+    def findBestItemToTake(self, playerLevel: int) -> str:
+        """
+        Find the best item to take according to the player level
+        """
+        # If there are multiple items with the same quantity, choose best item for player level
+        if playerLevel == 1:
+            return "linemate"
+        elif playerLevel == 2:
+            if self.agentInfo.inventory["linemate"] < 2:
+                return "linemate"
+            if self.agentInfo.inventory["deraumere"] < 1:
+                return "deraumere"
+            return "sibur"
+        elif playerLevel == 3:
+            if self.agentInfo.inventory["phiras"] < 2:
+                return "phiras"
+            if self.agentInfo.inventory["linemate"] < 2:
+                return "linemate"
+            return "sibur"
+        elif playerLevel == 4:
+            if self.agentInfo.inventory["sibur"] < 2:
+                return "sibur"
+            if self.agentInfo.inventory["phiras"] < 1:
+                return "phiras"
+            if self.agentInfo.inventory["deraumere"] < 1:
+                return "deraumere"
+            return "linemate"
+        elif playerLevel == 5:
+            if self.agentInfo.inventory["mendiane"] < 3:
+                return "mendiane"
+            if self.agentInfo.inventory["sibur"] < 1:
+                return "sibur"
+            if self.agentInfo.inventory["deraumere"] < 1:
+                return "deraumere"
+            return "linemate"
+        elif playerLevel == 6:
+            if self.agentInfo.inventory["sibur"] < 3:
+                return "sibur"
+            if self.agentInfo.inventory["phiras"] < 1:
+                return "phiras"
+            if self.agentInfo.inventory["deraumere"] < 2:
+                return "deraumere"
+            return "linemate"
+        elif playerLevel == 7:
+            if self.agentInfo.inventory["phiras"] < 2:
+                return "phiras"
+            if self.agentInfo.inventory["thystame"] < 1:
+                return "thystame"
+            if self.agentInfo.inventory["mendiane"] < 2:
+                return "mendiane"
+            if self.agentInfo.inventory["sibur"] < 2:
+                return "sibur"
+            if self.agentInfo.inventory["deraumere"] < 2:
+                return "deraumere"
+            return "linemate"
+        else:
+            return "linemate"
