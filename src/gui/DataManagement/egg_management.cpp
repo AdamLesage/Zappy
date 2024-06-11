ba92c6cd82f44d732bbd127fe2959a870e495454
mathieu-MZ <mathieu.mazeau@epitech.eu>
2024-06-11 10:29:43 +0200

    feat (egg management GUI): add function for the gg management

--- src/gui/DataManagement/egg_management.cpp
+++ src/gui/DataManagement/egg_management.cpp
@@ -9,21 +9,47 @@
 
 void GuiConnect::createEgg(std::vector<std::string> args)
 {
-    args = args;
+    std::shared_ptr<Zappy::Egg> egg = std::make_shared<Zappy::Egg>();
+    egg->_eggNb = std::stoi(args[1]);
+    egg->setPlayerNumber(std::stoi(args[2]));
+    egg->setPosition(std::stoi(args[3]), std::stoi(args[4]));
+    std::shared_ptr<Zappy::Player> playertt = std::make_shared<Zappy::Player>();
+    for (auto &player : this->_players) {
+        if (player->getPlayerNumber() == std::stoi(args[2])) {
+            playertt = player;
+            break;
+        }
+    }
+    egg->setTeamName(playertt->getTeamName());
+    this->_eggs.push_back(egg);
 }
 
 void GuiConnect::deleteEgg(std::vector<std::string> args)
 {
-    args = args;
+    for (std::size_t i = 0; i < this->_eggs.size(); i++) {
+        if (this->_eggs[i]->_eggNb == std::stoi(args[1])) {
+            this->_eggs.erase(this->_eggs.begin() + i);
+            return;
+        }
+    }
 }
 
 void GuiConnect::updateEgg(std::vector<std::string> args)
 {
-    args = args;
+    for (std::size_t i = 0; i < this->_eggs.size(); i++) {
+        if (this->_eggs[i]->_eggNb == std::stoi(args[1])) {
+            this->_eggs[i]->setPosition(std::stoi(args[2]), std::stoi(args[3]));
+            return;
+        }
+    }
 }
 
 std::shared_ptr<Zappy::Egg> GuiConnect::getEggByNb(int eggNb)
 {
-    eggNb = eggNb;
+    for (std::size_t i = 0; i < this->_eggs.size(); i++) {
+        if (this->_eggs[i]->_eggNb == eggNb) {
+            return this->_eggs[i];
+        }
+    }
     return nullptr;
 }
