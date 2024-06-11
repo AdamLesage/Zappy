86a1d0fd2b5bf0fee69c9ea3f4538b511c42aa27
AdamLesage <adamles44@gmail.com>
2024-06-11 11:23:46 +0200

    feat(CheckItems): add function which return which item searched is in player vision

--- src/ai/models/AgentMoves.py
+++ src/ai/models/AgentMoves.py
@@ -11,13 +11,25 @@ class Moves():
         """Ctor of the Moves class"""
         pass
     
-    def checkItem(self, lookResult: str, searchedItem):
+    def checkItem(self, lookResult: str, searchedItem) -> bool:
         """
         We pass an item (food, sibur, phiras...) to this function,
         with the string given by the look command.
         And it will return True if the item is present or false if not.
         """
         return True if searchedItem in lookResult else False
+    
+    def checkItems(self, lookResult: str, searchedItems: list[str]) -> list[str]:
+        """
+        We pass a list of items (food, sibur, phiras...) to this function,
+        with the string given by the look command.
+        And it will return a list of items that are present in the look result.
+        """
+        presentItems = []
+        for item in searchedItems:
+            if self.checkItem(lookResult, item):
+                presentItems.append(item)
+        return presentItems
     
     def reachItemList(self, itemToReach: str, lookResult: str):
         """
