e778785a3e994157f4da023a2319d6a8b41ead2e
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-06-16 16:54:57 +0200

    refactor (IEvulution): add methods to the IEvolution class

--- src/gui/Entity/IEvolution.hpp
+++ src/gui/Entity/IEvolution.hpp
@@ -28,8 +28,12 @@ class IEvolution {
         ~IEvolution() = default;
 
     // Getters
+    virtual std::pair<int, int> getPosition() const = 0;
+    virtual sf::Clock getClock() const = 0;
 
     // Setters
+    virtual void setPosition(int x, int y) = 0;
+    virtual void setClock(sf::Clock clock) = 0;
 
     // Member functions
 
