5886e6d5732ab54e103dd4e57256f4659e2cbab5
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-06-16 16:44:12 +0200

    refactor (IEvolution): create the IEvolution class

---
+++ src/gui/Entity/IEvolution.hpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** IEvolution
+*/
+
+#pragma once
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Audio/SoundBuffer.hpp>
+#include <SFML/Audio/Sound.hpp>
+#include <SFML/Window/Mouse.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
+#include <SFML/Window/Event.hpp>
+
+class IEvolution {
+    public:
+    // Ctor
+        IEvolution() = default;
+    
+    // Dtor
+        ~IEvolution() = default;
+
+    // Getters
+
+    // Setters
+
+    // Member functions
+
+    protected:
+    private:
+};
