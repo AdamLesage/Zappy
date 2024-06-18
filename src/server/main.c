6c6f758613118f050eb9bc2ecee43204da844b4d
rchevallier1234 <romain.chevallier@epitech.eu>
2024-06-18 10:40:56 +0200

    feat(Server Core): add buffer managment close #213

--- src/server/main.c
+++ src/server/main.c
@@ -12,14 +12,20 @@ void signal_handler(int sig)
     sig = sig;
 }
 
+void sigpipe_handler(int sig)
+{
+    sig = sig;
+}
+
 int main(const int argc, const char **argv)
 {
     core_t core;
 
     signal(SIGINT, signal_handler);
+    signal(SIGPIPE, sigpipe_handler);
     init_core(argc, argv, &core);
     init_server(&core);
-    lunch_server(&core);
+    lunch_server(&core); 
     close_server(&core);
     return (0);
 }
