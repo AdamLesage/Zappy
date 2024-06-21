c95a4d53d4bdd797fb118cfcf6e843c101292040
rchevallier1234 <romain.chevallier@epitech.eu>
2024-06-21 15:54:55 +0200

    fix(Server): fix the coding style for pdr command

--- src/server/Event/pdr.c
+++ src/server/Event/pdr.c
@@ -7,7 +7,8 @@
 
 #include "server.h"
 
-static void send_pdr_info(network_t *network, int id, enum Object object, int fd)
+static void send_pdr_info(network_t *network, int id,
+    enum Object object, int fd)
 {
     add_to_send_buffer(network, "pdr ", fd);
     add_int_to_send_buffer(network, id, fd);
@@ -21,7 +22,7 @@ void pdr(core_t *core, int id, enum Object object)
     for (players_list_t *tmp = core->players.players_list;
         tmp != NULL; tmp = tmp->next) {
         if (strcmp(tmp->player_info->team_name, "GRAPHIC") == 0) {
-            send_pdr_info(&core->network , id, object, tmp->fd);
+            send_pdr_info(&core->network, id, object, tmp->fd);
         }
     }
 }
