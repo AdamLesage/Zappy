2866ceabebe3fbb22e9369033cf816a126d4481a
Victor Hristea <victorhristea@gmail.com>
2024-06-21 10:58:32 +0200

    fix (GUI Interface): fix print team info position

--- src/gui/Interface/Broadcast.cpp
+++ src/gui/Interface/Broadcast.cpp
@@ -34,24 +34,23 @@ Zappy::Broadcast::~Broadcast()
 void Zappy::Broadcast::display(int i)
 {
     if (_clock.getElapsedTime().asSeconds() < 2) {
-        if (i < _broadcasts_sprites.size() && _broadcasts_sprites.size() > 0)
+        if (i < _broadcasts_sprites.size() && _broadcasts_sprites.size() > 0) {
             for (size_t j = 0; j < _broadcasts_sprites.size(); j++) {
                 _window->draw(_broadcasts_sprites[j]);
             }
+        }
     } else if (_clock.getElapsedTime().asSeconds() >= 2) {
         _broadcasts_sprites.clear();
-        printf("Broadcasts cleared\n");
     }
 }
 
 void Zappy::Broadcast::check_player_broadcast(int i)
 {
-    if (_guiConnect->_players[i]->getMessage() != "") {
+    if (i < _guiConnect->_players.size() && _guiConnect->_players[i]->getMessage() != "") {
         _broadcasts_sprites.push_back(sf::Sprite());
-        _broadcasts_sprites[_broadcasts_sprites.size() - 1].setScale(0.35, 0.1);
+        _broadcasts_sprites[_broadcasts_sprites.size() - 1].setScale(0.1, 0.1);
         _broadcasts_sprites[_broadcasts_sprites.size() - 1].setTexture(_broadcasts_textures[0]);
         _broadcasts_sprites[_broadcasts_sprites.size() - 1].setPosition(_guiConnect->_players[i]->getPosition()[0] * 102.4 + 100, (_guiConnect->_players[i]->getPosition()[1] + 1) * 102.4);
-        printf("Player %d broadcasted: %s\n", i, _guiConnect->_players[i]->getMessage().c_str());
         _guiConnect->_players[i]->setMessage("");
         _clock.restart();
     }
